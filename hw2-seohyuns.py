a502d012ef4e900b1d32b43a6088ca350baf1d96
Clara Shim <seohyuns@andrew.cmu.edu>
2016-07-15 20:09:00 +0900

    got a and transpose of a. time to find m

--- hw2-seohyuns.py
+++ hw2-seohyuns.py
@@ -4,11 +4,17 @@ import scipy as sp
 poll = open ('UN_voting_data.txt')
 l = [ map(str,line.split(' ')) for line in poll ]
 
+for i in range(len(l)):
+	del l[i][0]
+
+
 a = np.row_stack(l)
 trans = a.transpose()
 
-
+M= a * trans
+#np.dot(a,trans)
 
 print a
 print trans
+print M
 
